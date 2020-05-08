f9e8fc601ea9ba2a7629a34eb51cd4c583d0939f
bhushanblaster <55149243+bhushanblaster@users.noreply.github.com>
2020-05-09 01:11:24 +0530

    Create Check_If_It_Is_a_Straight_Line.cpp

---
+++ Check_If_It_Is_a_Straight_Line.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    double getSlope(int x1,int y1,int x2,int y2)
+    {
+        if(x2-x1 == 0)
+            return 0;
+        else
+            return (double)(y2-y1)/(x2-x1);
+    }
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+        if(coordinates.size()<=2)
+            return true;
+        else
+        {
+            auto p1 = coordinates[0];
+            auto p2 = coordinates[1];
+            double slope = getSlope(p1[0],p1[1],p2[0],p2[1]);
+            for(int i=2;i<coordinates.size();i++)
+            {
+                auto p3 = coordinates[i];
+                double slope1 = getSlope(p2[0],p2[1],p3[0],p3[1]);
+                if(slope1!=slope)
+                    return false;
+            }
+            return true;
+        }
+    }
+};
